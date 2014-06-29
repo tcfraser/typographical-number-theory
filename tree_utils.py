142d187241e0b0ab90f3643550c17bcb915b76f1
Jason Liu <jxnl@users.noreply.github.com>
2014-06-29 02:06:28 -0400

    Updated the use of None.
    `if var == None` is equivalent to `if not var`
    `if var != None` is equivalent to 'if var'
    
    The if statement can check for existence.
    
    Moreover,
    
    While I do not know what `print_term` does, consider using the `__repr__` or `__str__` method instead. this way, it overloads the print operator so you can just use 
    
    `str(Term_Object)`
    `print(Term_Object)`

--- tree_utils.py
+++ tree_utils.py
@@ -2,18 +2,19 @@ class Term(value, children, enclose):
     def __init__(self, value = None, children = None, enclose = None):
     	self.value = value
     	self.children = children
-    	if enclose = None:
-    		self.paren, self.opp = None
+    	if enclose:
+    	    self.paren, self.opp = enclose
     	else:
-    		self.paren = enclose[0]
-    		self.opp = enclose[1]
+    		self.paren, self.opp = None, None
 
     def add_child(self, new_children):
         self.children.append(new_children)
 
     def print_term(self, start = ""):
-    	if self.value != None:
+    	if self.value:
     		if self.value == "S":
+    
+
 
 
 
@@ -24,6 +25,6 @@ class Term(value, children, enclose):
 a = node("a")
 b = node("S", [a])
 
-test = node(None, [a,b], ["()", "+"])
+test = node(None, [a, b], ["()", "+"])
 
 term_1 =  
