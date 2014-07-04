0947b876f2a33a3897b377c5ffbf9bd3392f4891
tcfraser <tcfraser237@gmail.com>
2014-07-04 14:40:22 -0400

    Stuff

--- predicates.py
+++ predicates.py
@@ -8,8 +8,8 @@ import regex
 Numeral = r"S*0"
 Variable = r"[abcde]'*?"
 
-def Modifiers(i = [0]):
-    return regex.subf(r"Modifiers", "Modifiers" + indices(i), "(?P<Modifiers>"+ "(?:~|E"+ Variable + ":" + "|A" + Variable + ":" + ")*"  + ")")
+def Quantifiers(i = [0]):
+    return regex.subf(r"Quantifiers", "Quantifiers" + indices(i), "(?P<Quantifiers>"+ "(?:~|E"+ Variable + ":" + "|A" + Variable + ":" + ")*"  + ")")
 
 def indices(listofints):
     return "_" + "_".join(str(i) for i in listofints)
@@ -18,13 +18,16 @@ def Term(i = [0]):
     return regex.subf(r"Term", "Term" + indices(i), "(?P<Term>S*(?:\(((?&Term))[.+]((?&Term))\)" + "|" + Numeral + "|" + Variable + "))")  
 
 def wfs(i = [0]):
-    return regex.subf(r"wfs", "wfs" + indices(i), "(?P<wfs>" +  Modifiers([i[0],1]) + "(?:<((?&wfs))[&V-]((?&wfs))>" + "|" + Term([i[0],1]) + "=" + Term([i[0],2]) + "))")
+    return regex.subf(r"wfs", "wfs" + indices(i), "(?P<wfs>" +  Quantifiers([i[0],1]) + "(?:<((?&wfs))[&V-]((?&wfs))>" + "|" + Term([i[0],1]) + "=" + Term([i[0],2]) + "))")
 
 # the general wfs composed of two wfs
-general =  Modifiers([0]) + "(?P<lbra><)" + wfs([1]) + "(?P<opperator>[&V-])" + wfs([2]) +  "(?P<rbra>>)"
+general =  Quantifiers([0]) + "(?P<lbra><)" + wfs([1]) + "(?P<opperator>[&V-])" + wfs([2]) +  "(?P<rbra>>)"
 
 # Predicate to determine if string matches desired regular expression
 # regex string -> Boolean
 def isType (exp, string):
     return True if regex.fullmatch(exp, string) else False
+
+def isWFS (string):
+	return isType(wfs([0]), string)
         
