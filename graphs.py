7182a518017d9ad01d1f5fc78792f423f28b792a
tcfraser <tcfraser237@gmail.com>
2014-07-08 22:41:50 -0400

    Graph Visualization
    Learning how to Use Matplotlib and Networkx modules for python.

---
+++ graphs.py
@@ -0,0 +1,29 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G=nx.random_geometric_graph(600,0.1)
+# position is stored as node attribute data for random_geometric_graph
+pos=nx.get_node_attributes(G,'pos')
+
+# find node near center (0.5,0.5)
+dmin=1
+ncenter=0
+for n in pos:
+    x,y=pos[n]
+    d=(x-0.5)**2+(y-0.5)**2
+    if d<dmin:
+        ncenter=n
+        dmin=d
+
+# color by path length from node near center
+p=nx.single_source_shortest_path_length(G,ncenter)
+
+plt.figure(figsize=(8,8))
+nx.draw_networkx_edges(G,pos,nodelist=[ncenter], alpha=0.5)
+nx.draw_networkx_nodes(G,pos,nodelist=p.keys(), node_size=45, node_color=list(p.values()), cmap=plt.cm.rainbow)
+
+plt.xlim(-0.05,1.05)
+plt.ylim(-0.05,1.05)
+plt.axis('on')
+plt.savefig('random_geometric_graph.png')
+plt.show()
